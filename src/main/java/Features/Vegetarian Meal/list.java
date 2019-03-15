64a1abf32fae5eba34e99d97cf850a077c8e9bbd
Jari van den Broek <j.vandenbroek-2@student.tudelft.nl>
2019-03-15 08:10:39 +0000

    Upload New File

---
+++ src/main/java/Features/Vegetarian Meal/list.java
@@ -0,0 +1,31 @@
+
+public class list {
+	private String name;
+	private int value;
+	
+	public list(String name, int value) {
+		
+		this.username = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void Name(String name) {
+		this.name = name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+	
+
+}
