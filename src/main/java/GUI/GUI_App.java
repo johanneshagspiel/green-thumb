a6c7e6447aeee04e20bfb32d27b7df25bad81695
Ardyz <a.zwanenburg@student.tudelft.nl>
2019-03-15 10:03:39 +0100

    Created a two buttons, one substracts points the other gives points. It also live updates the score.

--- src/main/java/GUI/GUI_App.java
+++ src/main/java/GUI/GUI_App.java
@@ -24,11 +24,13 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class GUI_App {
 
 	private static JFrame frame;
 	private String username;
+	private static int points;
 
 	/**
 	 * Launch the application.
@@ -50,7 +52,7 @@ public class GUI_App {
 	 * Create the application.
 	 */
 	private GUI_App(String username) {
-		this.username = username;
+		this.setUsername(username);
 		initialize(username);
 	}
 
@@ -60,47 +62,89 @@ public class GUI_App {
 	private static void initialize(String username) {
 		// Create the frame
 		frame = new JFrame();
-		frame.setBounds(200, 200, 500, 300);
+		frame.getContentPane().setBackground(new Color(240, 240, 240));
+		frame.setBounds(200, 200, 1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		frame.setFocusable(true);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.getContentPane().setLayout(null);
 
 		// Labels
-		JLabel lblLogin = new JLabel("Welcome " + username);
-		lblLogin.setBounds(187, 28, 104, 30);
-		frame.getContentPane().add(lblLogin);
+		JLabel lblWelcome = new JLabel("Welcome " + username);
+		lblWelcome.setFont(new Font("Times New Roman", Font.PLAIN, 29));
+		lblWelcome.setBounds(10, 10, 1520, 59);
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblWelcome);
+		
+		points = 0;
+		JLabel lblPoints = new JLabel("Your points are " + points);
+		lblPoints.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblPoints.setBounds(10, 42, 1520, 50);
+		lblPoints.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblPoints);
 
 		// Buttons
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		btnLogin.setBounds(206, 202, 85, 21);
-		frame.getContentPane().add(btnLogin);
-
-		JButton btnReset = new JButton("Reset");
+		
+		// Vegetarian meal
+		JButton btnReset = new JButton("Vegetarian meal");
+		btnReset.setBounds(45, 170, 156, 30);
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+				points += 100;
+				lblPoints.setText("Your points are " + points);
 			}
 		});
-		btnReset.setBounds(111, 202, 85, 21);
 		frame.getContentPane().add(btnReset);
-
-		JButton btnExit = new JButton("Exit");
-		btnExit.addActionListener(new ActionListener() {
+		
+		JButton button = new JButton("Non-Vegetarian meal");
+		button.setBounds(202, 170, 156, 30);
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JFrame frame = new JFrame("Exit");
-				if (JOptionPane.showConfirmDialog(frame, "Confirm if you want to exit", "Login Systems",
+				points += -100;
+				lblPoints.setText("Your points are " + points);
+			}
+		});
+		frame.getContentPane().add(button);
+
+		//Labels
+		JLabel lblWhatKindOf = new JLabel("What kind of meal did you have today?");
+		lblWhatKindOf.setBounds(45, 140, 313, 30);
+		lblWhatKindOf.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblWhatKindOf);
+
+		// Creates the menu-bar
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		// Creates the "My Profile" in menu-bar
+		JMenuItem mntmMyProfile = new JMenuItem("My Profile");
+		menuBar.add(mntmMyProfile);
+
+		// Creates the "My Friends" in menu-bar
+		JMenuItem mntmMyFriends = new JMenuItem("My Friends");
+		menuBar.add(mntmMyFriends);
+
+		// Creates the "Log out" in menu-bar
+		JMenuItem mntmLogOut = new JMenuItem("Log out");
+		mntmLogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (JOptionPane.showConfirmDialog(frame, "Confirm if you want to exit", "Log out",
 						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
-					System.exit(0);
+					Login_S.main(null);
+					frame.setVisible(false);
 				}
 			}
 		});
-		btnExit.setBounds(301, 202, 85, 21);
-		frame.getContentPane().add(btnExit);
+		menuBar.add(mntmLogOut);
 	}
 
+	
+	//Getters and Setters username
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
