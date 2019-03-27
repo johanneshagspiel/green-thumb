55d322960ca2e051750b08cbfdad4b130c88548f
Ardyz <a.zwanenburg@student.tudelft.nl>
2019-03-27 18:19:06 +0100

    Testing Client/src/main/java/helper/ComparatorLeaderBoard.jave (now 100% coverage)

--- src/Client/src/main/java/GUI/GUI_App.java
+++ src/Client/src/main/java/GUI/GUI_App.java
@@ -7,19 +7,11 @@ import mainscreen.FriendsProfile;
 import service.UserServiceImpl;
 import mainscreen.MyProfile;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
 
 
 /**
