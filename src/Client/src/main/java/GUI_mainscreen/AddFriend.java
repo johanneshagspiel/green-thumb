f6ee90d7ae57b74a13490aa851d99be641c07ed0
Ardyz <a.zwanenburg@student.tudelft.nl>
2019-03-28 14:46:16 +0100

    Creating the GUI for add friends

--- src/Client/src/main/java/GUI_mainscreen/AddFriend.java
+++ src/Client/src/main/java/GUI_mainscreen/AddFriend.java
@@ -12,6 +12,7 @@ import java.awt.*;
 public class AddFriend {
 
     private static JFrame frame3;
+    private static JTextField txtUsername;
 
     private AddFriend(User userIn, UserServiceImpl clientIn) {
         initialize(userIn, clientIn);
@@ -45,20 +46,54 @@ public class AddFriend {
     public static void initialize(User userIn, UserServiceImpl clientIn) {
 
         frame3 = new JFrame();
-        frame3.setBounds(300, 500, 300, 500);
-        frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame3.setBounds(300, 300, 300, 300);
         frame3.getContentPane().setLayout(null);
-        frame3.setFocusable(true);
-
-        // Labels
-        JLabel lblWelcome = new JLabel("My Profile");
-        lblWelcome.setBounds(120, 20, 150, 20);
-        frame3.getContentPane().add(lblWelcome);
-
-        JLabel lblUsername = new JLabel("Your Username " + userIn.getUser_name());
-        lblUsername.setBounds(120, 10, 10, 20);
+        
+        JLabel lblAddFriend = new JLabel("Add Friend");
+        lblAddFriend.setHorizontalAlignment(SwingConstants.CENTER);
+        lblAddFriend.setFont(new Font("Arial Black", Font.PLAIN, 16));
+        lblAddFriend.setBounds(10, 20, 266, 31);
+        frame3.getContentPane().add(lblAddFriend);
+        
+        JSeparator separator = new JSeparator();
+        separator.setBounds(20, 49, 256, 2);
+        frame3.getContentPane().add(separator);
+        
+        txtUsername = new JTextField();
+        txtUsername.setBounds(122, 71, 118, 19);
+        frame3.getContentPane().add(txtUsername);
+        txtUsername.setColumns(10);
+        
+        JLabel lblUsername = new JLabel("Username");
+        lblUsername.setFont(new Font("Arial Black", Font.PLAIN, 12));
+        lblUsername.setBounds(30, 74, 82, 13);
         frame3.getContentPane().add(lblUsername);
+        
+        JButton btnSearch = new JButton("Search");
+        btnSearch.setBounds(155, 100, 85, 21);
+        frame3.getContentPane().add(btnSearch);
+        
+        JLabel lblResults = new JLabel("Results");
+        lblResults.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        lblResults.setBounds(30, 144, 85, 19);
+        frame3.getContentPane().add(lblResults);
+        
+        JSeparator separator_1 = new JSeparator();
+        separator_1.setBounds(20, 131, 256, 2);
+        frame3.getContentPane().add(separator_1);
 
+        for(int i = 0; i<3; i++) {
+        	JButton btnAdd = new JButton("Add");
+            btnAdd.setBounds(191, 175+i*25, 85, 21);
+            frame3.getContentPane().add(btnAdd);
+            
+            JLabel labelAdd = new JLabel("<name>");
+            labelAdd.setFont(new Font("Arial Black", Font.PLAIN, 12));
+            labelAdd.setBounds(30, 175+i*25, 130, 19);
+            frame3.getContentPane().add(labelAdd);
+        }
+        
+        frame3.setFocusable(true);
 
     }
 }
