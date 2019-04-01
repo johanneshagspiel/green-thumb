d43e601c849cdfae7ca3d539a888723ae6e2b19c
Jari van den Broek <j.vandenbroek-2@student.tudelft.nl>
2019-04-01 23:22:43 +0000

    Update Checkstyle LeaderBoardChoice.java 8 errors left

--- src/Client/src/main/java/GUI_mainscreen/LeaderBoardChoice.java
+++ src/Client/src/main/java/GUI_mainscreen/LeaderBoardChoice.java
@@ -1,6 +1,6 @@
 package GUI_mainscreen;
 
-import GUI.GUI_App;
+
 import entity.LeaderBoardEntry;
 import entity.User;
 import service.UserServiceImpl;
@@ -9,26 +9,42 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static GUI.GUI_App.getUsername;
+
 import static GUI.GUI_App.setLabel;
 
 /**
  * The type My profile.
  */
 public class LeaderBoardChoice {
-
+/** Initialising the frame. */
     private static JFrame frame2;
 
-    private LeaderBoardChoice(JLabel lblLeaderboardContent, JLabel lblLeaderboard, User userIn) {
+    /**
+     * The Leaderboard.
+     * @param lblLeaderboardContent the content
+     * @param lblLeaderboard the leaderboard
+     * @param userIn the user
+     */
+    private LeaderBoardChoice(final JLabel lblLeaderboardContent,
+                              final JLabel lblLeaderboard, final User userIn) {
         initialize(lblLeaderboardContent, lblLeaderboard, userIn);
     }
 
-
-    public static void application(JLabel lblLeaderboardContent, JLabel lblLeaderboard, User userIn) {
+    /**
+     * The application.
+     * @param lblLeaderboardContent the content
+     * @param lblLeaderboard the leaderboard
+     * @param userIn the user
+     */
+    public static void application(final JLabel lblLeaderboardContent,
+                                   final JLabel lblLeaderboard,
+                                   final User userIn) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    LeaderBoardChoice window = new LeaderBoardChoice(lblLeaderboardContent, lblLeaderboard, userIn);
+                    LeaderBoardChoice window
+                            = new LeaderBoardChoice(lblLeaderboardContent,
+                            lblLeaderboard, userIn);
                     LeaderBoardChoice.frame2.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -37,26 +53,55 @@ public class LeaderBoardChoice {
         });
     }
 
-    public static void initialize(JLabel lblLeaderboardContent, JLabel lblLeaderboard, User userIn) {
+    /**
+     * Initialising.
+     * @param lblLeaderboardContent the content
+     * @param lblLeaderboard the leaderboard
+     * @param userIn the user
+     */
+    public static void initialize(final JLabel lblLeaderboardContent,
+                                  final JLabel lblLeaderboard, final User userIn) {
+        final int threezerozero = 300;
+        final int fivezerozero = 500;
+        final int onezero = 10;
+        final int twosixsix = 266;
+        final int twozero = 20;
+        final int fivezero = 50;
+        final int sevenfive = 75;
+        final int onezerozero = 100;
+        final int onetwofive = 125;
+        final int onefivezero = 150;
+        final int onesevenfive = 175;
+        final int twozerozero = 200;
+        final int twotwofive = 225;
+        final int twofivezero = 250;
 
         frame2 = new JFrame();
-        frame2.setBounds(300, 500, 300, 500);
+        frame2.setBounds(threezerozero, fivezerozero,
+                threezerozero, fivezerozero);
         frame2.getContentPane().setLayout(null);
         frame2.setFocusable(true);
 
-        JLabel lblWelcome = new JLabel("What do you want to depict?");
+        JLabel lblWelcome
+                = new JLabel("What do you want to depict?");
         lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-        lblWelcome.setBounds(10, 10, 266, 20);
+        lblWelcome.setBounds(onezero, onezero,
+                twosixsix, twozero);
         frame2.getContentPane().add(lblWelcome);
 
-        JButton lblVegetarian = new JButton("Vegetarian Score");
+        JButton lblVegetarian
+                = new JButton("Vegetarian Score");
         lblVegetarian.setHorizontalAlignment(SwingConstants.CENTER);
-        lblVegetarian.setBounds(10, 50, 266, 20);
+        lblVegetarian.setBounds(onezero,
+                fivezero, twosixsix, twozero);
         lblVegetarian.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                String label = lblVegetarian.getText();
+            public void actionPerformed(final ActionEvent arg0) {
+                String label
+                        = lblVegetarian.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.
+                        setText(LeaderBoardEntry.
+                                createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Vegetarian Score");
                 frame2.setVisible(false);
             }
@@ -65,12 +110,14 @@ public class LeaderBoardChoice {
 
         JButton lblProduce = new JButton("Produce Score");
         lblProduce.setHorizontalAlignment(SwingConstants.CENTER);
-        lblProduce.setBounds(10, 75, 266, 20);
+        lblProduce.setBounds(onezero, sevenfive,
+                twosixsix, twozero);
         lblProduce.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblProduce.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Produce Score");
                 frame2.setVisible(false);
             }
@@ -79,26 +126,32 @@ public class LeaderBoardChoice {
 
         JButton lblCar = new JButton("Car Score");
         lblCar.setHorizontalAlignment(SwingConstants.CENTER);
-        lblCar.setBounds(10, 100, 266, 20);
+        lblCar.setBounds(onezero, onezerozero, twosixsix, twozero);
         lblCar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblCar.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Car Score");
                 frame2.setVisible(false);
             }
         });
         frame2.getContentPane().add(lblCar);
 
-        JButton lblPublic_transportation = new JButton("Public Transportation Score");
-        lblPublic_transportation.setHorizontalAlignment(SwingConstants.CENTER);
-        lblPublic_transportation.setBounds(10, 125, 266, 20);
-        lblPublic_transportation.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+        JButton lblPublic_transportation
+                = new JButton("Public Transportation Score");
+        lblPublic_transportation.
+                setHorizontalAlignment(SwingConstants.CENTER);
+        lblPublic_transportation.
+                setBounds(onezero, onetwofive, twosixsix, twozero);
+        lblPublic_transportation.
+                addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblPublic_transportation.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Top 10 Public Transportation Score");
                 frame2.setVisible(false);
             }
@@ -107,12 +160,13 @@ public class LeaderBoardChoice {
 
         JButton lblBike = new JButton("Bike Score");
         lblBike.setHorizontalAlignment(SwingConstants.CENTER);
-        lblBike.setBounds(10, 150, 266, 20);
+        lblBike.setBounds(onezero, onefivezero, twosixsix, twozero);
         lblBike.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblBike.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Bike Score");
                 frame2.setVisible(false);
             }
@@ -121,26 +175,30 @@ public class LeaderBoardChoice {
 
         JButton lblTemperature = new JButton("Temperature Score");
         lblTemperature.setHorizontalAlignment(SwingConstants.CENTER);
-        lblTemperature.setBounds(10, 175, 266, 20);
+        lblTemperature.setBounds(onezero, onesevenfive,
+                twosixsix, twozero);
         lblTemperature.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblTemperature.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Temperature Score");
                 frame2.setVisible(false);
             }
         });
         frame2.getContentPane().add(lblTemperature);
 
-        JButton lblSolar = new JButton("Solar Score" );
+        JButton lblSolar = new JButton("Solar Score");
         lblSolar.setHorizontalAlignment(SwingConstants.CENTER);
-        lblSolar.setBounds(10, 200, 266, 20);
+        lblSolar.setBounds(onezero, twozerozero,
+                twosixsix, twozero);
         lblSolar.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblSolar.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 Solar Score");
                 frame2.setVisible(false);
             }
@@ -149,12 +207,14 @@ public class LeaderBoardChoice {
 
         JButton lblSaved = new JButton("Overall CO2 Saved");
         lblSaved.setHorizontalAlignment(SwingConstants.CENTER);
-        lblSaved.setBounds(10, 225, 266, 20);
+        lblSaved.setBounds(onezero, twotwofive,
+                twosixsix, twozero);
         lblSaved.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblSaved.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 CO2 Saved Score");
                 frame2.setVisible(false);
             }
@@ -163,12 +223,14 @@ public class LeaderBoardChoice {
 
         JButton lblOverall = new JButton("Overall Score");
         lblOverall.setHorizontalAlignment(SwingConstants.CENTER);
-        lblOverall.setBounds(10, 250, 266, 20);
+        lblOverall.setBounds(onezero, twofivezero,
+                twosixsix, twozero);
         lblOverall.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
+            public void actionPerformed(final ActionEvent arg0) {
                 String label = lblOverall.getText();
                 setLabel(label);
-                lblLeaderboardContent.setText(LeaderBoardEntry.createLeaderboard(userIn, label));
+                lblLeaderboardContent.setText(LeaderBoardEntry.
+                        createLeaderboard(userIn, label));
                 lblLeaderboard.setText("Leaderboard Top 10 CO2 Used");
                 frame2.setVisible(false);
             }
