fe0f0830585386ac52b9a315d93393e6876b1873
Jari van den Broek <j.vandenbroek-2@student.tudelft.nl>
2019-04-03 19:18:18 +0000

    Update Checkstyle SortLength.java 1091 left, class 100% checkstyle

--- src/Client/src/main/java/helper/SortLength.java
+++ src/Client/src/main/java/helper/SortLength.java
@@ -4,9 +4,17 @@ import java.util.Comparator;
 
 import entity.User;
 
-public class SortLength implements Comparator<User>{
-
-	public int compare(User a, User b) {
+/**
+ * The length sort.
+ */
+public class SortLength implements Comparator<User> {
+    /**
+     * The compare.
+     * @param a the user a
+     * @param b the user b
+     * @return the result
+     */
+    public final int compare(final User a, final User b) {
         return a.getUser_name().length() - (b.getUser_name().length());
     }
 }
