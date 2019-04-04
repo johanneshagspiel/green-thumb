acc7dcf9e2de84f50eb7d343218926133953f691
Jari van den Broek <j.vandenbroek-2@student.tudelft.nl>
2019-04-04 14:45:52 +0000

    Update Checkstyle package-info.java 375 errors

--- src/Server/TestServer/src/main/java/com/luv2code/springdemo/config/package-info.java
+++ src/Server/TestServer/src/main/java/com/luv2code/springdemo/config/package-info.java
@@ -1,2 +1,5 @@
+/** This package contains the classes to connect this program to hibernate. */
 package com.luv2code.springdemo.config;
-// This package contains the classes to connect this program to hibernate
+
+
+
