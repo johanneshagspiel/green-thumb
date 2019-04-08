3eed0f5cbfeb4e68fcc8e0287830d4d66cd4d7c5
Ardyz <a.zwanenburg@student.tudelft.nl>
2019-04-08 11:24:52 +0200

    Following TA tips adding more test, coverage is still 66,7%

--- src/Client/src/test/java/helper/PointsMissingTest.java
+++ src/Client/src/test/java/helper/PointsMissingTest.java
@@ -9,6 +9,8 @@ import helper.PointsMissing;
 public class PointsMissingTest {
 	
 	final int result = 544;
+	final int thousand = 1000;
+	final int minusresult = 1456;
 
 	@Test
 	public void testUnderThousand() {
@@ -24,6 +26,46 @@ public class PointsMissingTest {
 		int pointsIn = 12456;
 		
 		assertEquals( PointsMissing.determiningPoints(pointsIn), result );
+	}
+	
+	@Test
+	public void testEqualThousand() {
+		
+		int pointsIn = 1000;
+		
+		assertEquals( PointsMissing.determiningPoints(pointsIn), thousand );
+	}
+	
+	@Test
+	public void testEqualZero() {
+		
+		int pointsIn = 0;
+		
+		assertEquals( PointsMissing.determiningPoints(pointsIn), thousand );
+	}
+	
+	@Test
+	public void testUnderMinusThousand() {
+		
+		int pointsIn = -456;
+		
+		assertEquals( PointsMissing.determiningPoints(pointsIn), minusresult );
+	}
+	
+	@Test
+	public void testAboveMinusThousand() {
+		
+		int pointsIn = -12456;
+		
+		assertEquals( PointsMissing.determiningPoints(pointsIn), minusresult );
+	}
+	
+	@Test
+	public void testEqualMinusThousand() {
+		
+		int pointsIn = -1000;
+		
+		assertEquals( PointsMissing.determiningPoints(pointsIn), thousand );
 	}
 
 }
