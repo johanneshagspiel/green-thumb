08d5d31b74309bf10f846468d2247f49f31d732c
Lukas Zimmerhackl <L.K.Zimmerhackl@student.tudelft.nl>
2019-04-09 18:19:37 +0200

    I split createLeaderBoard up in 2 functions and added tests

--- src/Client/src/test/java/entity/LeaderBoardEntryTest.java
+++ src/Client/src/test/java/entity/LeaderBoardEntryTest.java
@@ -1,11 +1,57 @@
 package entity;
 
-import entity.LeaderBoardEntry;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static entity.LeaderBoardEntry.userSupplyEntry;
 import static org.junit.Assert.*;
 
 public class LeaderBoardEntryTest {
+    User karel = new User();
+    Friend karelFriend = new Friend();
+    User lukas = new User();
+    @Before public void initialize(){
+
+        karel.setVegetarian(100);
+        karel.setUser_name("karel");
+        karel.setPassword("asdf");
+        karel.setTotal(1000);
+        karel.setTotal_saved(1000);
+        karel.setProduce(101);
+        karel.setSolar(102);
+        karel.setTemperature(103);
+        karel.setBike(104);
+        karel.setCar(105);
+        karel.setPublic_transportation(106);
+        karel.setId(107);
+        karel.setFriendlist(null);
+
+
+        karelFriend.setId(1);
+        karelFriend.setUser_name_entry("karel");
+        karelFriend.setFriend_name("lukas");
+
+
+        lukas.setVegetarian(100);
+        lukas.setUser_name("lukas");
+        lukas.setPassword("asdf");
+        lukas.setTotal(1000);
+        lukas.setTotal_saved(1000);
+        lukas.setProduce(101);
+        lukas.setSolar(102);
+        lukas.setTemperature(103);
+        lukas.setBike(104);
+        lukas.setCar(105);
+        lukas.setPublic_transportation(106);
+        lukas.setId(107);
+        List<Friend> friends = new ArrayList<Friend>();
+        friends.add(karelFriend);
+        lukas.setFriendlist(friends);
+    }
 	
 	@Test
 	public void test_Constructor() {
@@ -47,5 +93,91 @@ public class LeaderBoardEntryTest {
     	assertTrue(Ardy.toString().equals("Ardy, 9001"));
     }
 
-    
+    @Test
+    public void testUserSupplyEntryVegetarian(){
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 100);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Vegetarian Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryTopTen(){
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 1000);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Leaderboard Top 10 CO2 Used");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryOverallScore(){
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 1000);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Overall Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryProduceScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 101);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Produce Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryCarScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 105);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Car Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryPublTransportScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 106);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Public Transportation Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryBikeScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 104);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Bike Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryTemperatureScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 103);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Temperature Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntrySolarScore() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 102);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Solar Score");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryOverallCO2Saved() {
+        LeaderBoardEntry entry = new LeaderBoardEntry("You", 1000);
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "Overall CO2 Saved");
+        assertEquals(entry.toString(), supplied.toString());
+    }
+
+    @Test
+    public void testUserSupplyEntryElse(){
+        LeaderBoardEntry entry = null;
+
+        LeaderBoardEntry supplied = userSupplyEntry(lukas, "asdfsadfsadffadf");
+        assertEquals(entry, supplied);
+    }
 }
