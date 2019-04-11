f3b0603bc22c27506d9c602a61cdbd8e38c291cd
Jari van den Broek <j.vandenbroek-2@student.tudelft.nl>
2019-04-11 23:47:33 +0000

    Update Checkstyle Login_addAccount.java 225 errors left

--- src/Client/src/main/java/Login/Login_addAccount.java
+++ src/Client/src/main/java/Login/Login_addAccount.java
@@ -4,23 +4,19 @@ import GUI.Login_S;
 import entity.User;
 import service.UserServiceImpl;
 import javax.swing.*;
-
 /**
  * The type Login add account.
  */
 public class Login_addAccount extends Login_S {
-	
+	/** the add account. */
 	public Login_addAccount() {
-		
 	}
-	
 	/**
 	 * Creates a new account Pre.
 	 *  - Username is not used yet - Username and Password
 	 * are not empty Result - Username
 	 * and Password are stored in "loginDetails.txt"
 	 * Return - Created account boolean
-	 *
 	 * @param username    the username
 	 * @param password    the password
 	 * @param userIn      the user in
@@ -41,21 +37,19 @@ public class Login_addAccount extends Login_S {
 			final JTextField txtPassword,
 			final JTextField txtRPassword,
 			final JFrame frame) {
-
 		// Check if password and username textfields are not empty
+
 		if (password.isEmpty() || username.isEmpty()) {
 			JOptionPane.showMessageDialog(
 					null,
 					"To create an account,"
-							+ " please enter an "
-							+ "username and password",
+							+ " please enter an username and password",
 					"Create account",
 					JOptionPane.ERROR_MESSAGE);
 			txtUsername.setText(null);
 			txtPassword.setText(null);
 			return false;
 		}
-
 		// Check if the username is available
 		if (clientIn.getUser2(username) != null) {
 			JOptionPane.showMessageDialog(null,
