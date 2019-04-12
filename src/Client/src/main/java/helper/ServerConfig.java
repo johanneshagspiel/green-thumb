a6d8b519e5a99e7bfc1ca9a4377f1fbcd5014675
nnathanordonez <N.A.NathanOrdonez@student.tudelft.nl>
2019-04-12 14:31:24 +0200

    Turns out the port was 443, and I was missing a "friend" page on the helper class I made.

--- src/Client/src/main/java/helper/ServerConfig.java
+++ src/Client/src/main/java/helper/ServerConfig.java
@@ -3,10 +3,11 @@ package helper;
 public class ServerConfig {
 
     //declaring the variables
-    private static String homepage = "https://area22gogreen.herokuapp.com:8080";
+    private static String homepage = "https://area22gogreen.herokuapp.com:443";
     private static String apipage = homepage + "/api/";
     private static String userpage = apipage + "users/";
     private static String friendspage = apipage + "friends/";
+    private static String friendpage = apipage + "friend/";
 
     /**
      * Returning the correct homepage URL for the TomCat instance the app should connect to.
