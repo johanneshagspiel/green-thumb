b6e4c86d618f02974741943f3efa8c0caf849e54
Lukas Zimmerhackl <L.K.Zimmerhackl@student.tudelft.nl>
2019-04-13 22:39:38 +0200

    added the before Test thing

---
+++ src/Client/src/test/java/helper/ChatContentTest.java
@@ -0,0 +1,39 @@
+package helper;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import service.UserServiceImpl;
+import entity.User;
+
+import javax.swing.*;
+
+import static helper.ChatContent.createChat;
+import static helper.ChatContent.postComment;
+import static org.junit.Assert.*;
+
+public class ChatContentTest {
+    private User chatcontent = null;
+    private UserServiceImpl userService = new UserServiceImpl();
+
+    /*
+        saves what you hate in your chat
+        and overwrites it with the test chat
+     */
+    @Before
+    public void saveChatContent(){
+        chatcontent = userService.getUser(999);
+        User testUser = new User();
+        testUser.setId(999);
+        testUser.setUser_name("Global Chat");
+        String chat = "Global Chat: Be the first to post in the chat!" +
+                "SplitHereer" +
+                "Lukas: HelloSplitHereer" +
+                "Karel: Hi, how much CO2 did you save?SplitHereer" +
+                "Lukas: 69kilo's!SplitHereer" +
+                "Karel: Ha, noob, I saved 420 kilo'sSplitHereer" +
+                "Lukas: dayumnSplitHereer";
+        testUser.setPassword(chat);
+        userService.updateUser(testUser);
+    }
+}
